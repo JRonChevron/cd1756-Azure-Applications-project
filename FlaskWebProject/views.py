76637ad86f99857bc8c9546ca0b81d255ede91a4
JRonChevron <juan.ron@chevron.com>
2025-05-16 15:33:39 -0300

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -118,13 +118,13 @@ def _load_cache():
     cache = msal.SerializableTokenCache()
     if session.get("token_cache"):
         cache.deserialize(session["token_cache"])
-    return cache
+    return cache
 
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
     if cache.has_state_changed:
-        session["token_cache"] = cache.serialize()
+        session["token_cache"] = cache.serialize()
 
 
 def _build_msal_app(cache=None, authority=None):
